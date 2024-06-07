cf286494dcb0bc1a37d95db512f3183b38b618c5
Subhasis Sahoo <subhasis.0891@gmail.com>
2024-06-08 04:30:22 +0530

    Update bootstrap.sh

--- kubeadm-ha-keepalived-haproxy/external-keepalived-haproxy/bootstrap.sh
+++ kubeadm-ha-keepalived-haproxy/external-keepalived-haproxy/bootstrap.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# Enable ssh password authentication
-echo "[TASK 1] Enable ssh password authentication"
-sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
-echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
-systemctl reload sshd
+# Enable ssh password authentication and ChallengeResponseAuthentication
+echo "[TASK 1] Enable ssh password authentication and ChallengeResponseAuthentication"
+sudo sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sudo sed -i 's/^PermitRootLogin .*/PermitRootLogin yes/' /etc/ssh/sshd_config
+sudo sed -i 's/^ChallengeResponseAuthentication .*/ChallengeResponseAuthentication yes/' /etc/ssh/sshd_config
+sudo systemctl reload sshd
 
 # Set Root password
 echo "[TASK 2] Set root password"
-echo -e "kubeadmin\nkubeadmin" | passwd root >/dev/null 2>&1
+echo -e "kubeadmin\nkubeadmin" | sudo passwd root >/dev/null 2>&1
